91d1d326412cb8379fc4b497eada8f064317f3aa
Shehan Lakshitha <shehanlaksh@gmail.com>
2023-08-26 16:28:09 +0530

    Added home section

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
-import './Home.css'
-import imageBg from '../../Assets/Images/body.jpg'
+import React from "react";
+import "./Home.css";
+import imageBg from "../../Assets/Images/body.jpg";
+
+import { AiOutlineArrowRight } from "react-icons/ai";
 
 const Home = () => {
   return (
-    <div className='imageBg'>
-      <img src={imageBg} alt="" />
+    <div className="Home">
+      <div className="imageBg">
+        <img src={imageBg} alt="" />
+      </div>
+      <div className="sectionText">
+        <h1>Get the Genuie Spare Parts</h1>
+        <p>We provide you the best spare parts fit for your vehicle for the lowest price in the market</p>
+        <button className="btn flex">View products <AiOutlineArrowRight className='icon'/> </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
